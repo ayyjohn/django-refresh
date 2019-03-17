101be003e1e7bcce3c7b1ffa218ec5de30c25eb8
Alec Johnson <alecjohnson55@gmail.com>
2019-03-17 11:03:58 -0700

    fix typo in on_delete

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,6 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delet=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes=models.IntegerField(defalt=0)
